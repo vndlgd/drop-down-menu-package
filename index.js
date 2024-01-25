c2c5e84b4ba6d01dfa541cd6db3d06237ef8ee3a
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2024-01-24 20:40:29 -0800

    Fix dropdown menu hiding issue on self-click and refine styling

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ const dropBtn = document.querySelectorAll('.dropbtn');
 // open/close drop down menu on btn clicked
 dropBtn.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    // close other menus and display one at a time
-    dropBtn.forEach((btn) => {
-      btn.nextElementSibling.classList.remove('visible');
-    });
-    // open close menu upon click
-    btn.nextElementSibling.classList.toggle('visible');
-    // prevents window event listener from being called
-    e.stopPropagation();
+    // if element we click on is already visible, we close it
+    /* allows us to close menu on button click
+     while still displaying one menu at a time */
+    if (e.target.nextElementSibling.classList.contains('visible')) {
+      e.target.nextElementSibling.classList.remove('visible');
+    } else {
+      // close other menus and display one at a time
+      dropBtn.forEach((btn) => {
+        btn.nextElementSibling.classList.remove('visible');
+      });
+
+      // open close menu upon click
+      btn.nextElementSibling.classList.toggle('visible');
+
+      // prevents window event listener from being called
+      e.stopPropagation();
+    }
   });
 });
 
@@ -19,10 +28,8 @@ window.addEventListener('click', (e) => {
   if (e.target !== dropBtn) {
     dropBtn.forEach((btn) => {
       if (btn.nextElementSibling.classList.contains('visible')) {
-        console.log(btn.nextElementSibling);
         btn.nextElementSibling.classList.remove('visible');
       }
     });
   }
 });
-
